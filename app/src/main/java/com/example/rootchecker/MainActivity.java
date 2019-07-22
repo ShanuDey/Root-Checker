02f32946c47857e634dcfd82cb8594beac2f221d
Shanu Dey <shanudey1999@gmail.com>
2019-07-22 20:33:34 +0530

    su command to check root accesss

--- app/src/main/java/com/example/rootchecker/MainActivity.java
+++ app/src/main/java/com/example/rootchecker/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.rootchecker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
@@ -13,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
     private Button btn_check_root;
     private LinearLayout linearLayout;
     private boolean isRooted;
+    private static final String superUserPermission = "su";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,22 +26,52 @@ public class MainActivity extends AppCompatActivity {
         linearLayout = findViewById(R.id.ll_background);
 
 
+
+
         btn_check_root.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                isRooted = RootUtil.isDeviceRooted();
-                if(isRooted){
-                    tv_status.setText("Rooted");
-                    linearLayout.setBackground(getDrawable(R.color.green));
-                }
-                else{
-                    tv_status.setText("Not Rooted");
-                    linearLayout.setBackground(getDrawable(R.color.red));
-                }
-
+//                isRooted = RootUtil.isDeviceRooted();
+//                if(isRooted){
+//                    setStatus(isRooted);
+//                }
+                isRooted = executeShellCommand(superUserPermission);
+                setStatus(isRooted);
             }
         });
 
+    }
+
+    public void setStatus(boolean isRooted){
+        if(isRooted){
+            tv_status.setText("Rooted");
+            linearLayout.setBackground(getDrawable(R.color.green));
+        }
+        else{
+            tv_status.setText("Not Rooted");
+            linearLayout.setBackground(getDrawable(R.color.red));
+        }
+    }
+
+    private boolean executeShellCommand(String command){
+        Process process = null;
+        try{
+            process = Runtime.getRuntime().exec(command);
+            //isRooted =true;
+            Log.v("shanu","try rooted");
+            return true;
+        } catch (Exception e) {
+            //isRooted = false;
+            Log.v("shanu","catch not rooted");
+            return false;
+        } finally{
+            if(process != null){
+                try{
+                    process.destroy();
+                }catch (Exception e) {
+                }
+            }
+        }
     }
 
 
